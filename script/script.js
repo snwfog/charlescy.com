e5c674e4a167756e787e081bdaa3ecaad38654bd
snw <donchoa@gmail.com>
2012-01-07 13:40:17 -0500

    Improve scroll top
    Add smoothing, and css transition.

--- script/script.js
+++ script/script.js
@@ -1,9 +1,24 @@
 // Scroll to top
 $(document).ready(function() {
-	$('#go-top').click(function() {
-		$('body, html').animate({
-			scrollTop: 0
-		}, 800);
-		return false;
+	// hide #go-top at initial position
+	$('#go-top').hide();
+	
+	// fade in #go-top
+	$(function() {
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > 100) {
+				$('#go-top').fadeIn();
+			} else {
+				$('#go-top').fadeOut();
+			}
+		});
+		
+		// scroll to 0px on click
+		$('#go-top').click(function() {
+			$('body, html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
 	});
 });
